27bf1c25b9db4668673c96370ca038a62f822dec
Andrey Mishchenko <buryonok@gmail.com>
2021-02-26 10:24:46 +0300

    Set svg use

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,50 @@
 const { src, dest, parallel, series, watch } = require('gulp');
+const del = require('del');
 const pug = require('gulp-pug');
+const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
+const svgSprite = require('gulp-svg-sprite');
+const imagemin = require('gulp-imagemin');
 
-function buildHtml() {
-  return src('app/*.pug')
+const config = {
+  mode: {
+    stack: {
+      sprite: "../sprite.svg"
+    }
+  }
+};
+
+const cleanDist = () => del('build/**/*', { force: true });
+
+const buildHtml = () => src('app/*.pug')
   .pipe(pug({
     pretty: true
   }))
   .pipe(dest('build'));
-};
 
-function buildCss() {
-	return src(['app/scss/variables.scss', 'app/scss/general.scss', 'app/scss/header.scss', 'app/scss/footer.scss'])
+const buildCss = () => src(['app/scss/app.scss'])
+  .pipe(sourcemaps.init())
   .pipe(sass())
   .pipe(concat('style.css'))
 	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(dest('build'))
-}
+  .pipe(sourcemaps.write())
+	.pipe(dest('build'));
 
-function startWatch() {
+const buildSvg = () => src('app/images/icons/*.svg')
+  .pipe(svgSprite(config))
+  .pipe(dest('build/images'));
+
+const buildImages = () => src('app/images/*.*')
+  .pipe(imagemin())
+  .pipe(dest('build/images'));
+
+const startWatch = () => {
 	watch('app/**/*.pug', buildHtml);
   watch('app/scss/*.scss', buildCss);
+  watch('app/images/icons/*.svg', buildSvg);
+  watch('app/images/*', buildImages);
 };
 
-exports.default = series(parallel(buildHtml, buildCss), startWatch);
+exports.default = series(cleanDist, parallel(buildHtml, buildCss, buildSvg, buildImages), startWatch);
